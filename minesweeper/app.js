99667bd8a2390ca8e31e71c0d02577687939ab4c
Lucas <126298728+LucasFagundesWielewski@users.noreply.github.com>
2024-05-06 22:11:36 -0300

    start of minesweeper

---
+++ minesweeper/app.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    const flagsLeft = document.querySelector('#flags-left')
+    const result = document.querySelector('#result')
+    let width = 10
+    let bombAmount = 10
+    let squares = []
+    let gameOver
+})
