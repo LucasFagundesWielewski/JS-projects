733e4336aed6072f02964527b11ab0d190ee2954
Lucas <126298728+LucasFagundesWielewski@users.noreply.github.com>
2024-05-07 21:39:35 -0300

    The half of the project

--- minesweeper/app.js
+++ minesweeper/app.js
@@ -5,5 +5,39 @@ document.addEventListener('DOMContentLoaded', () => {
     let width = 10
     let bombAmount = 10
     let squares = []
-    let gameOver
+    let isGameOver = false
+
+    function createBoard() {
+        flagsLeft.innerHTML = bombAmount
+
+        const bombsArray = Array(bombAmount).fill('bomb')
+        const emptyArray = Array(width*width - bombAmount).fill('valid')
+        const gameArray = emptyArray.concat(bombsArray)
+        const shuflledArray = gameArray.sort(() => Math.random() -0.5 )
+
+        for(let i = 0;  i < width*width; i++) {
+            const square = document.createElement('div')
+            square.setAttribute('id', i)
+            square.classList.add(shuflledArray[i])
+            grid.appendChild(square)
+            square.push(square)
+
+            square.addEventListener('click', function(e) {
+                this.click(square)
+            })
+
+            square.oncontextmenu = function(e) {
+                e.preventDefault()
+            }
+        }
+
+        for (let i = 0; i < squares.length; i++) {
+            let total = 0
+            const isLeftEdge = (i % width === 0)
+            const isRightEdge = (i % width === width - 1)
+            if (squares[i].classList.contains('valid')) {
+                
+            }
+        }
+    }
 })
